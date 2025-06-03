3f511432e23da3ebbaf3584530efd0096b53c068
Andrew Ferreira <andrew.shien2@gmail.com>
2025-06-02 23:51:01 -0300

    fix: remove svg size class

--- src/components/button-icon/ButtonIcon.vue
+++ src/components/button-icon/ButtonIcon.vue
@@ -1,20 +1,39 @@
 <script setup lang="ts" generic="T extends string">
 import { cn } from '../../utils';
-import type { VNode } from 'vue';
 import { Button } from '../button';
+import { computed, type VNode } from 'vue';
 import { ButtonLink } from '../button-link';
 import type { ButtonIconProps } from './types';
 
 const props = defineProps<ButtonIconProps<T>>();
 
 defineSlots<{ default?: () => VNode }>();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+  return delegated;
+});
 </script>
 
 <template>
-  <ButtonLink v-if="to" :to variant="ghost" size="icon" :class="props.class" :button-class>
+  <ButtonLink
+    v-if="to"
+    v-bind="delegatedProps"
+    :to
+    variant="ghost"
+    size="icon"
+    :class="props.class"
+    :button-class
+  >
     <component :is="icon" :class="iconClass" :size="iconSize" />
   </ButtonLink>
-  <Button v-else variant="ghost" size="icon" :class="cn(props.class, buttonClass)">
+  <Button
+    v-else
+    v-bind="delegatedProps"
+    variant="ghost"
+    size="icon"
+    :class="cn(props.class, buttonClass)"
+  >
     <component :is="icon" :class="iconClass" :size="iconSize" />
   </Button>
 </template>
