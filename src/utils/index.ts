7f3a1047bc7569eae2c3c97e03827a86801c76fb
Andrew Ferreira <andrew.shien2@gmail.com>
2025-12-01 23:18:30 -0300

    fix: export `ClassValue`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 import { twMerge } from 'tailwind-merge';
 import { type ClassValue, clsx } from 'clsx';
 
+export type { ClassValue };
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
