f32271683ee479cb4529c957080f5308c455573b
Andrew Ferreira <andrew.shien2@gmail.com>
2026-05-03 13:57:54 -0300

    chore: update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { join } from 'node:path';
-import dts from 'vite-plugin-dts';
+import dts from 'unplugin-dts/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [vue(), tailwind(), dts({ rollupTypes: false })],
+  plugins: [
+    vue(),
+    tailwind(),
+    dts({ rollupTypes: false }),
+  ],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
